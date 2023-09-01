e44b6cf407b3e45d627c9d4613ed1138732a5ab3
Pankaj Bajpai <bajpaipankaj@google.com>
2023-09-01 17:30:23 -0400

    Addition code lab 3

--- samples/snippets/src/main/java/com/example/spanner/ObservabilityLab.java
+++ samples/snippets/src/main/java/com/example/spanner/ObservabilityLab.java
@@ -18,14 +18,15 @@ import java.util.concurrent.Executors;
  * simulate these behaviour.
  */
 public class ObservabilityLab {
-  private static final String LOREM_IPSUM =
-      "Aenean sit amet lectus est. Nullam ornare ligula luctus auctor placerat. Morbi fermentum volutpat massa, sit amet consectetur metus vehicula sed. Sed auctor scelerisque tempus. Morbi hendrerit tortor et felis scelerisque, at fermentum risus posuere. Suspendisse finibus, tellus eu ullamcorper posuere, odio ex pulvinar est, a malesuada libero turpis quis massa. Quisque placerat lacus orci. Proin ac viverra metus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus cursus rhoncus iaculis. Pellentesque nisl tellus, venenatis nec erat sit amet, eleifend porttitor nisl. Maecenas fringilla ex id mauris facilisis, sed luctus dui elementum. Suspendisse ac orci lectus. Suspendisse vitae sapien convallis, commodo leo ut, ultricies arcu. Fusce pellentesque sem vestibulum, sodales purus eget, auctor odio. Ut et nunc metus. Aenean ac ex faucibus, tristique nibh ut, euismod lorem. Fusce a ex ut nibh consectetur mollis. Aenean suscipit elit dui, faucibus vestibulum leo commodo a. Nulla ultricies vitae velit cursus commodo. Morbi eu sapien in magna condimentum porta quis eget sem. Etiam tempor auctor diam, quis mollis odio scelerisque et. Fusce tempus mauris mi, et varius enim condimentum in. Aliquam nisi lorem, pulvinar id ullamcorper vitae, fringilla vel leo. Fusce vehicula tincidunt vulputate. Vivamus efficitur nunc quis lorem porttitor elementum. Donec ex neque, vestibulum nec mollis quis, lacinia quis dui. Nullam rhoncus quis lacus nec euismod. Vivamus porttitor sem nec nisl auctor ultrices. Vivamus non laoreet lectus. Aliquam condimentum semper libero eu elementum. Nullam lobortis ultricies gravida. Integer in lacinia lacus, ac consequat magna. Suspendisse et risus vel diam facilisis ornare a in arcu. Nulla nec nunc sem. Cras aliquam nulla sem, luctus maximus est gravida ut. Ut pellentesque pharetra convallis. Quisque molestie, ipsum sit amet scelerisque convallis, magna ante fringilla massa, in blandit turpis nibh ornare magna. Curabitur mi tortor, feugiat id sem ac, scelerisque congue tortor. Aenean non viverra risus. Praesent vel enim quis dolor auctor aliquet. Maecenas faucibus mi at venenatis suscipit. Integer interdum magna vitae mauris interdum, laoreet ullamcorper erat tincidunt. Morbi vel ipsum convallis, semper quam vitae, sagittis tellus. In facilisis eu lorem imperdiet laoreet. Suspendisse gravida a magna et condimentum. Suspendisse vitae risus vitae est pulvinar convallis at sit amet sem. Morbi vel imperdiet leo, sit amet cursus urna. Pellentesque suscipit ut neque non laoreet. Duis non ipsum ipsum. Quisque ut porttitor dui. Duis nulla augue, varius quis tellus sed, efficitur bibendum justo. Cras vestibulum congue ante in gravida. Quisque tincidunt nisi nisl, sed vulputate leo luctus ac. Pellentesque quis tempor leo, sed faucibus ipsum. Donec rutrum turpis nec auctor lobortis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed lobortis sodales risus vitae eleifend. Nullam pharetra dapibus lobortis. Vestibulum nisl diam, dignissim sit amet lacus sit amet, venenatis auctor nisi. Phasellus sollicitudin tortor a mi fringilla, imperdiet tristique ex feugiat. Nulla ut vehicula metus, nec tempus odio. Phasellus pellentesque lacus lacus, fringilla egestas felis hendrerit ac. Nam suscipit orci eros, at molestie nunc tincidunt id. Aliquam maximus finibus leo. Vestibulum orci turpis, fringilla eget lorem id, cursus consequat tortor. Suspendisse efficitur purus lectus, ac ornare sem lobortis a. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sodales id mi eu aliquam. Curabitur enim libero, tempor vel vestibulum id, eleifend at lacus. Maecenas ut feugiat justo, nec dictum justo. Nulla porttitor accumsan rhoncus. Cras ligula velit, molestie sit amet molestie sed, commodo et libero. Etiam eu condimentum arcu. Aliquam erat volutpat. Nullam sit amet urna ipsum. Vestibulum at velit id velit tincidunt ultricies a ut metus. In vitae mollis diam. Proin lacinia fringilla purus vitae aliquet. Vestibulum a tincidunt eros. Aenean imperdiet aliquet arcu, vitae euismod turpis fermentum quis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse potenti. Pellentesque quis efficitur mauris, pulvinar sagittis ante. Cras efficitur porta convallis. Nullam eleifend congue finibus. Nam sodales tincidunt odio ac elementum. Sed sem risus, imperdiet quis suscipit eu, imperdiet vitae nibh. Morbi gravida neque ac sodales varius. In convallis massa vel lectus fermentum ultricies ac vitae eros. Sed justo sem, dignissim sit amet tempus ultrices, pellentesque at libero. Quisque a quam volutpat, tristique erat a, auctor enim. Suspendisse finibus arcu erat, nec mattis tortor facilisis in. Donec ornare mattis lorem, in interdum turpis venenatis vel. Mauris tempus porttitor libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus eget tempor dui. Nunc et elit eu eros tincidunt sapien.";
-
-  private static final String SUBJECT = "Random subject for the email for testing purpose %s";
-
   private static final int NUM_PROCESSORS = Runtime.getRuntime().availableProcessors();
 
-  public void createMessages(DatabaseAdminClient dbAdminClient, DatabaseId id) {
+  /**
+   * This method creates Message table which will be used for monotonically increasing writes.
+   *
+   * @param dbAdminClient DatabaseAdminClient
+   * @param id Database object
+   */
+  public void createMessageForIncreasingWrites(DatabaseAdminClient dbAdminClient, DatabaseId id) {
     createDatabase(
         dbAdminClient,
         id,
@@ -36,18 +37,34 @@ public class ObservabilityLab {
                 + ") PRIMARY KEY (msg_id)"));
   }
 
-  public void writeMessagesParallel(
+  /** Calling the method for parallel execution */
+  public void performMonotonicallyIncreasingWritesParallel(
       DatabaseClient dbClient, int mutationsPerTransaction, int numMinutes) {
     executeTasksInParallel(
-        () -> writeMessages(dbClient, mutationsPerTransaction, numMinutes), NUM_PROCESSORS);
+        () -> performMonotonicallyIncreasingWrite(dbClient, mutationsPerTransaction, numMinutes),
+        NUM_PROCESSORS);
   }
 
-  public void writeMessagesParallelUUID(
+  /**
+   * Calling the method for parallel execution
+   *
+   * @param dbClient DatabaseClient
+   * @param mutationsPerTransaction mutations per transaction.
+   * @param numMinutes How long this method should execute in minutes.
+   */
+  public void performMultiParticipantWriteParallel(
       DatabaseClient dbClient, int mutationsPerTransaction, int numMinutes) {
     executeTasksInParallel(
-        () -> writeMessagesUUID(dbClient, mutationsPerTransaction, numMinutes), NUM_PROCESSORS);
+        () -> performMultiParticipantWrite(dbClient, mutationsPerTransaction, numMinutes),
+        NUM_PROCESSORS);
   }
 
+  /**
+   * This method creates Mailbox and Message table which are interleaved.
+   *
+   * @param dbAdminClient DatabaseAdminClient
+   * @param id Database object
+   */
   public void createInterleaved(DatabaseAdminClient dbAdminClient, DatabaseId id) {
     createDatabase(
         dbAdminClient,
@@ -58,27 +75,36 @@ public class ObservabilityLab {
                 + "state STRING(MAX),"
                 + ") PRIMARY KEY (sid)",
             "CREATE TABLE Message("
-                + " sid INT64 NOT NULL,"
-                + " msg_id STRING(MAX) NOT NULL,"
-                + " subject STRING(MAX),"
-                + " body STRING(MAX),"
-                + " send_timestamp TIMESTAMP  OPTIONS (allow_commit_timestamp=true),"
+                + "sid INT64 NOT NULL,"
+                + "msg_id STRING(MAX) NOT NULL,"
+                + "body STRING(MAX),"
+                + "send_timestamp TIMESTAMP  OPTIONS (allow_commit_timestamp=true),"
                 + ") PRIMARY KEY (sid, msg_id),"
                 + "INTERLEAVE IN PARENT Mailbox ON DELETE CASCADE"));
   }
 
+  /** Writing the value in the Mailbox table. */
   public void writeMailboxes(DatabaseClient dbClient, int numMailboxes) {
     List<Mutation> mutations = new ArrayList<>();
     for (int sid = 0; sid < numMailboxes; ++sid) {
       mutations.add(Mutation.newInsertBuilder("Mailbox").set("sid").to(sid).build());
     }
     try {
-      dbClient.write(mutations);
+      dbClient.writeWithOptions(
+          mutations, Options.tag("app=lab3,env=dev,case=writeMailBox,action=insert"));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
+  /**
+   * Parallel writing the values in the Messages table. Here Message table is interleaved in
+   * Mailbox.
+   *
+   * @param numMailboxes number of Mailbox to be used to write values in interleaves Message table.
+   * @param mutationsPerTransaction number of Mutations per transaction
+   * @param numMinutes how long it should write.
+   */
   public void writeMessagesInterleavedParallel(
       DatabaseClient dbClient, int numMailboxes, int mutationsPerTransaction, int numMinutes) {
     executeTasksInParallel(
@@ -86,6 +112,12 @@ public class ObservabilityLab {
         NUM_PROCESSORS);
   }
 
+  /**
+   * This method creates the WorkList table.
+   *
+   * @param dbAdminClient DatabaseAdminClient
+   * @param id Database object
+   */
   public void createWorkItems(DatabaseAdminClient dbAdminClient, DatabaseId id) {
     createDatabase(
         dbAdminClient,
@@ -98,11 +130,22 @@ public class ObservabilityLab {
                 + ") PRIMARY KEY (id)"));
   }
 
+  /**
+   * This method inserts data in the worklist table which can also be called as work items
+   *
+   * @param dbClient DatabaseClient
+   * @param mutationsPerTransaction mutations per transaction.
+   * @param numWorkItems Number of work items to be written in the table. This is basically the
+   *     number of rows should be inserted in the worklist table.
+   */
   public void writeWorkItems(
       DatabaseClient dbClient, int numWorkItems, int mutationsPerTransaction) {
+    // Finding the number of transaction to run the outer loop
     int numTransactions = numWorkItems / mutationsPerTransaction;
+
     for (int i = 0; i < numTransactions; ++i) {
       List<Mutation> mutations = new ArrayList<>();
+
       for (int j = 0; j < mutationsPerTransaction; ++j) {
         mutations.add(
             Mutation.newInsertBuilder("WorkList")
@@ -115,19 +158,30 @@ public class ObservabilityLab {
                 .build());
       }
       try {
-        dbClient.write(mutations);
+        dbClient.writeWithOptions(
+            mutations, Options.tag("app=lab3,env=dev,case=WorkListItem,action=insert"));
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
     }
   }
 
+  /** Calling the method for parallel execution */
   public void doWorkSingleTransactionParallel(DatabaseClient dbClient, boolean isLocking) {
     executeTasksInParallel(
         () -> doWorkSingleTransactionSerial(dbClient, isLocking), NUM_PROCESSORS);
   }
 
-  private void writeMessages(DatabaseClient dbClient, int mutationsPerTransaction, int numMinutes) {
+  /**
+   * This method writes the data into the Message table. Here primary key will be monotonically
+   * increasing.
+   *
+   * @param dbClient DatabaseClient
+   * @param mutationsPerTransaction mutations per transaction.
+   * @param numMinutes How long this method should execute in minutes.
+   */
+  private void performMonotonicallyIncreasingWrite(
+      DatabaseClient dbClient, int mutationsPerTransaction, int numMinutes) {
     Instant doneTime = Instant.now().plus(numMinutes, ChronoUnit.MINUTES);
     while (Instant.now().isBefore(doneTime)) {
       List<Mutation> mutations = new ArrayList<>();
@@ -137,42 +191,54 @@ public class ObservabilityLab {
                 .set("msg_id")
                 .to(Instant.now().toString() + i + Thread.currentThread().getId())
                 .set("body")
-                .to(LOREM_IPSUM)
+                .to(String.format("test-body-%s", i))
                 .build());
       }
       try {
-        dbClient.write(mutations);
+        dbClient.writeWithOptions(
+            mutations,
+            // Tag has a limit of 50 characters hence keeping the tag shorts
+            Options.tag("app=lab3,env=dev,case=incremental,action=insert"));
       } catch (Exception e) {
         throw new RuntimeException(e);
       }
     }
   }
 
-  private void writeMessagesUUID(
+  /**
+   * Below method inserts the data in the table using random UUID value of primary. This method is
+   * expected to generate a mutli participant write in the table. Initial few minutes are required
+   * for warm up after which spanner may split the tablet into to or more. Here body is kept smaller
+   * so that rows are written faster, and we can see the expected behaviour.
+   *
+   * @param dbClient DatabaseClient
+   * @param mutationsPerTransaction mutations per transaction.
+   * @param numMinutes How long this method should execute in minutes.
+   */
+  public void performMultiParticipantWrite(
       DatabaseClient dbClient, int mutationsPerTransaction, int numMinutes) {
     Instant doneTime = Instant.now().plus(numMinutes, ChronoUnit.MINUTES);
     while (Instant.now().isBefore(doneTime)) {
-      List<Mutation> mutations = new ArrayList<>();
+      List<Mutation> mutationsMessage = new ArrayList<>();
       for (int i = 0; i < mutationsPerTransaction; ++i) {
-        mutations.add(
+        mutationsMessage.add(
             Mutation.newInsertBuilder("Message")
                 .set("msg_id")
                 .to(UUID.randomUUID().toString())
                 .set("body")
-                .to(LOREM_IPSUM)
+                .to(String.format("test-body-%s", i))
                 .build());
       }
-      try {
-        dbClient.write(mutations);
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
+      dbClient.writeWithOptions(
+          mutationsMessage, Options.tag("app=lab3,env=dev,case=multi-part,action=insert"));
     }
   }
 
+  /** Insert rows to Mailbox and Message tables which are interleaved */
   private void writeMessagesInterleaved(
       DatabaseClient dbClient, int numMailboxes, int mutationsPerTransaction, int numMinutes) {
     Instant doneTime = Instant.now().plus(numMinutes, ChronoUnit.MINUTES);
+
     while (Instant.now().isBefore(doneTime)) {
       for (int mailbox = 0; mailbox < numMailboxes; ++mailbox) {
         List<Mutation> mutations = new ArrayList<>();
@@ -183,16 +249,15 @@ public class ObservabilityLab {
                   .to(mailbox)
                   .set("msg_id")
                   .to(UUID.randomUUID().toString())
-                  .set("subject")
-                  .to(String.format(SUBJECT, i))
                   .set("body")
-                  .to(LOREM_IPSUM)
+                  .to(String.format("test-body-%s", i))
                   .set("send_timestamp")
                   .to(Value.COMMIT_TIMESTAMP)
                   .build());
         }
         try {
-          dbClient.write(mutations);
+          dbClient.writeWithOptions(
+              mutations, Options.tag("app=lab3,env=dev,case=wr-interleave,action=insert"));
         } catch (Exception e) {
           throw new RuntimeException(e);
         }
@@ -244,6 +309,13 @@ public class ObservabilityLab {
     }
   }
 
+  /**
+   * Method to execute the transaction locking or non locking method.
+   *
+   * @param dbClient DatabaseClient
+   * @param isLocking value to decide which method to call. True means locking method will be
+   *     called, false means the otherwise.
+   */
   private void doWorkSingleTransactionSerial(DatabaseClient dbClient, boolean isLocking) {
     boolean workRemaining = true;
     while (workRemaining) {
